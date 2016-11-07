2100e80e309c7ce090bd0a77c7ac4770d1d4a9c1
Sudhakar <nsudhakar@gmail.com>
2016-11-07 12:55:21 -0600

    Fixed imports

--- src/main/java/com/myretail/service/ProductService.java
+++ src/main/java/com/myretail/service/ProductService.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
